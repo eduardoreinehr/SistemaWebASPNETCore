df5e6ce58687b984c4dcdfa271d25fa726fdbadf
eduardoreinehr <eduardo_bigliassi@hotmail.com>
2019-10-25 16:41:15 -0300

    Sistema aula 2510
    aula 25-10, usando git e banco de dados na nuvem

---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class Produtos
+    {
+        public int ProdCodigo { get; set; }
+        public string ProdName { get; set; }
+        public decimal ProdPreco { get; set; }
+        public int ProdQtde { get; set; }
+    }
+}
---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/sistemawebduContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class sistemawebduContext : DbContext
+    {
+        public sistemawebduContext()
+        {
+        }
+
+        public sistemawebduContext(DbContextOptions<sistemawebduContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCodigo);
+
+                entity.Property(e => e.ProdCodigo).HasColumnName("Prod_Codigo");
+
+                entity.Property(e => e.ProdName)
+                    .IsRequired()
+                    .HasColumnName("Prod_Name")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(14, 2)");
+
+                entity.Property(e => e.ProdQtde).HasColumnName("Prod_Qtde");
+            });
+        }
+    }
+}
